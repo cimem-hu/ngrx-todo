9be1603a0ed94fc007b4818c7b1f6503fc0439ea
Roland Nagy <roland@cimem.hu>
2024-03-09 19:57:53 +0100

    feat(fe): loading and error

--- todo-app/src/app/todo/store/todo.selectors.ts
+++ todo-app/src/app/todo/store/todo.selectors.ts
@@ -3,3 +3,5 @@ import { AppStore } from "../../app.state";
 export const getTodos = (state: AppStore) => state.todo.todos;
 export const countTodosDone = (state: AppStore) => state.todo.todos.filter((todo) => todo.done).length;
 export const countTodosNotDone = (state: AppStore) => state.todo.todos.filter((todo) => !todo.done).length;
+export const isLoading = (state: AppStore) => state.todo.isLoading;
+export const hasError = (state: AppStore) => state.todo.error !== "";
--- todo-app/src/app/todo/todo.component.ts
+++ todo-app/src/app/todo/todo.component.ts
@@ -6,7 +6,7 @@ import { TodoListItemComponent } from "./todo-list-item.component";
 import { TodoCountComponent } from "./todo-count.component";
 import { TodoInputComponent } from "./todo-input.component";
 import { AppStore } from "../app.state";
-import { getTodos } from "./store/todo.selectors";
+import { getTodos, hasError, isLoading } from "./store/todo.selectors";
 
 export type Todo = {
   id: number;
@@ -19,14 +19,21 @@ export type Todo = {
   standalone: true,
   imports: [CommonModule, TodoListItemComponent, TodoCountComponent, TodoInputComponent],
   template: `
+    @if(hasError$ | async) {
+    <h1>Something went wrong!</h1>
+    } @else { @if(isLoading$ | async) {
+    <h3>Loading...</h3>
+    }
     <app-todo-count></app-todo-count>
     <app-todo-input></app-todo-input>
     @for (todo of todos$ | async; track todo.id) {
     <app-todo-list-item [todo]="todo"></app-todo-list-item>
-    }
+    } }
   `
 })
 export class TodoComponent {
   private readonly store = inject(Store<AppStore>);
   todos$ = this.store.select(getTodos);
+  isLoading$ = this.store.select(isLoading);
+  hasError$ = this.store.select(hasError);
 }
