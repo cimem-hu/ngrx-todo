00277a4a971835e3126db8e317dace694757cf89
Roland Nagy <roland@cimem.hu>
2024-03-09 19:38:16 +0100

    refactor(fe) variable names

--- todo-app/src/app/todo/store/todo.selectors.ts
+++ todo-app/src/app/todo/store/todo.selectors.ts
@@ -1,5 +1,5 @@
 import { AppStore } from "../../app.state";
 
 export const getTodos = (state: AppStore) => state.todo.todos;
-export const getTodosDone = (state: AppStore) => state.todo.todos.filter((todo) => todo.done).length;
-export const getTodosNotDone = (state: AppStore) => state.todo.todos.filter((todo) => !todo.done).length;
+export const countTodosDone = (state: AppStore) => state.todo.todos.filter((todo) => todo.done).length;
+export const countTodosNotDone = (state: AppStore) => state.todo.todos.filter((todo) => !todo.done).length;
--- todo-app/src/app/todo/todo-count.component.ts
+++ todo-app/src/app/todo/todo-count.component.ts
@@ -3,18 +3,18 @@ import { CommonModule } from "@angular/common";
 import { Store } from "@ngrx/store";
 
 import { AppStore } from "../app.state";
-import { getTodosDone, getTodosNotDone } from "./store/todo.selectors";
+import { countTodosDone, countTodosNotDone } from "./store/todo.selectors";
 
 @Component({
   selector: "app-todo-count",
   standalone: true,
   imports: [CommonModule],
-  template: ` <h2>Todos to finish: {{ notDoneCount$ | async }}</h2>
-    <h3>Todos done: {{ doneCount$ | async }}</h3>`
+  template: ` <h2>Todos to finish: {{ countNotDone$ | async }}</h2>
+    <h3>Todos done: {{ countDone$ | async }}</h3>`
 })
 export class TodoCountComponent {
   private readonly store = inject(Store<AppStore>);
 
-  notDoneCount$ = this.store.select(getTodosNotDone);
-  doneCount$ = this.store.select(getTodosDone);
+  countNotDone$ = this.store.select(countTodosDone);
+  countDone$ = this.store.select(countTodosNotDone);
 }
