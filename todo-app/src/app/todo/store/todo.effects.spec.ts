c194de2880ec619d6b2d2e95bafb29c66db0ae63
Rolcho <roland@cimem.hu>
2024-03-11 15:46:59 +0100

    jest -> vitest

--- todo-app/src/app/todo/store/todo.effects.spec.ts
+++ todo-app/src/app/todo/store/todo.effects.spec.ts
@@ -1,4 +1,5 @@
-import { of, throwError } from "rxjs";
+import { beforeEach, describe, expect, it, vitest } from "vitest";
+import { lastValueFrom, of, throwError } from "rxjs";
 import { take } from "rxjs/operators";
 import { Actions } from "@ngrx/effects";
 import { TodoService } from "../todo.service";
@@ -19,158 +20,144 @@ import {
 import { TodoEffects } from "./todo.effects";
 
 describe("Todo Effects", () => {
-  let todoService: jest.Mocked<TodoService>;
+  let todoService: TodoService;
   let actions$: Actions;
   let todoEffects: TodoEffects;
 
   describe("Add Todo", () => {
     beforeEach(() => {
       todoService = {
-        addTodo: jest.fn()
-      } as unknown as jest.Mocked<TodoService>;
+        addTodo: vitest.fn()
+      } as unknown as TodoService;
 
       const newTodo = { name: "Test Todo", done: false };
       actions$ = new Actions(of(addTodoStarted(newTodo)));
       todoEffects = new TodoEffects(actions$, todoService);
     });
 
-    it("should dispatch addTodoSuccess when todoService.addTodo is successful", (done) => {
+    it("should dispatch addTodoSuccess when todoService.addTodo is successful", async () => {
       const todo = { id: 1, name: "Test Todo", done: false };
       const outcome = addTodoSuccess(todo);
 
-      todoService.addTodo.mockReturnValue(of(todo));
+      todoService.addTodo = vitest.fn().mockReturnValue(of(todo));
 
       const action = todoEffects.handleAddTodoSideEffects$;
       action.pipe(take(1)).subscribe((outputActions) => {
         expect(outputActions).toEqual(outcome);
-        done();
       });
     });
 
-    it("should dispatch addTodoSuccess when todoService.addTodo fails", (done) => {
+    it("should dispatch addTodoError when todoService.addTodo fails", async () => {
       const error = new Error("Error");
       const outcome = addTodoError({ message: error.message });
 
-      todoService.addTodo.mockReturnValue(throwError(() => error));
+      todoService.addTodo = vitest.fn().mockReturnValue(throwError(() => error));
 
       const action = todoEffects.handleAddTodoSideEffects$;
-      action.pipe(take(1)).subscribe((outputActions) => {
-        expect(outputActions).toEqual(outcome);
-        done();
-      });
+      const outputAction = await lastValueFrom(action.pipe(take(1)));
+      expect(outputAction).toEqual(outcome);
     });
   });
 
   describe("Get Todos", () => {
     beforeEach(() => {
       todoService = {
-        getTodos: jest.fn()
-      } as unknown as jest.Mocked<TodoService>;
+        getTodos: vitest.fn()
+      } as unknown as TodoService;
 
       actions$ = new Actions(of(getTodosStarted()));
       todoEffects = new TodoEffects(actions$, todoService);
     });
 
-    it("should dispatch getTodosSuccess when todoService.getTodos is successful", (done) => {
+    it("should dispatch getTodosSuccess when todoService.getTodos is successful", async () => {
       const todo = { id: 1, name: "Test Todo", done: false };
       const outcome = getTodosSuccess({ todos: [todo] });
 
-      todoService.getTodos.mockReturnValue(of([todo]));
+      todoService.getTodos = vitest.fn().mockReturnValue(of([todo]));
 
       const action = todoEffects.handleGetTodosSideEffects$;
-      action.pipe(take(1)).subscribe((outputActions) => {
-        expect(outputActions).toEqual(outcome);
-        done();
-      });
+      const outputAction = await lastValueFrom(action.pipe(take(1)));
+      expect(outputAction).toEqual(outcome);
     });
 
-    it("should dispatch getTodosError when todoService.getTodos fails", (done) => {
+    it("should dispatch getTodosError when todoService.getTodos fails", async () => {
       const error = new Error("Error");
       const outcome = getTodosError({ message: error.message });
 
-      todoService.getTodos.mockReturnValue(throwError(() => error));
+      todoService.getTodos = vitest.fn().mockReturnValue(throwError(() => error));
 
       const action = todoEffects.handleGetTodosSideEffects$;
-      action.pipe(take(1)).subscribe((outputActions) => {
-        expect(outputActions).toEqual(outcome);
-        done();
-      });
+      const outputAction = await lastValueFrom(action.pipe(take(1)));
+      expect(outputAction).toEqual(outcome);
     });
   });
 
   describe("Remove Todo", () => {
     beforeEach(() => {
       todoService = {
-        removeTodo: jest.fn()
-      } as unknown as jest.Mocked<TodoService>;
+        removeTodo: vitest.fn()
+      } as unknown as TodoService;
 
       const todo = { id: 1 };
       actions$ = new Actions(of(removeTodoStarted(todo)));
       todoEffects = new TodoEffects(actions$, todoService);
     });
 
-    it("should dispatch removeTodoSuccess when todoService.removeTodo is successful", (done) => {
+    it("should dispatch removeTodoSuccess when todoService.removeTodo is successful", async () => {
       const todo = { id: 1 };
       const outcome = removeTodoSuccess(todo);
 
-      todoService.removeTodo = jest.fn().mockReturnValue(of(null));
+      todoService.removeTodo = vitest.fn().mockReturnValue(of(null));
 
       const action = todoEffects.handleRemoveTodoSideEffects$;
-      action.pipe(take(1)).subscribe((outputActions) => {
-        expect(outputActions).toEqual(outcome);
-        done();
-      });
+      const outputAction = await lastValueFrom(action.pipe(take(1)));
+      expect(outputAction).toEqual(outcome);
     });
 
-    it("should dispatch removeTodoError when todoService.removeTodo fails", (done) => {
+    it("should dispatch removeTodoError when todoService.removeTodo fails", async () => {
       const error = new Error("Error");
       const outcome = removeTodoError({ message: error.message });
 
-      todoService.removeTodo.mockReturnValue(throwError(() => error));
+      todoService.removeTodo = vitest.fn().mockReturnValue(throwError(() => error));
 
       const action = todoEffects.handleRemoveTodoSideEffects$;
-      action.pipe(take(1)).subscribe((outputActions) => {
-        expect(outputActions).toEqual(outcome);
-        done();
-      });
+      const outputAction = await lastValueFrom(action.pipe(take(1)));
+      expect(outputAction).toEqual(outcome);
     });
   });
 
   describe("Toggle Todo", () => {
     beforeEach(() => {
       todoService = {
-        toggleTodo: jest.fn()
-      } as unknown as jest.Mocked<TodoService>;
+        toggleTodo: vitest.fn()
+      } as unknown as TodoService;
 
       const todo = { id: 1, done: false };
       actions$ = new Actions(of(toggleTodoStarted(todo)));
       todoEffects = new TodoEffects(actions$, todoService);
     });
 
-    it("should dispatch toggleTodoSuccess when todoService.toggleTodo is successful", (done) => {
+    it("should dispatch toggleTodoSuccess when todoService.toggleTodo is successful", async () => {
       const updatedTodo = { id: 1, done: true };
       const outcome = toggleTodoSuccess(updatedTodo);
 
-      todoService.toggleTodo = jest.fn().mockReturnValue(of(updatedTodo));
+      todoService.toggleTodo = vitest.fn().mockReturnValue(of(updatedTodo));
 
       const action = todoEffects.handleToggleTodoSideEffects$;
-      action.pipe(take(1)).subscribe((outputActions) => {
-        expect(outputActions).toEqual(outcome);
-        done();
-      });
+      const outputAction = await lastValueFrom(action.pipe(take(1)));
+      expect(outputAction).toEqual(outcome);
     });
 
-    it("should dispatch toggleTodoError when todoService.toggleTodo fails", (done) => {
+    it("should dispatch toggleTodoError when todoService.toggleTodo fails", async () => {
       const error = new Error("Error");
       const outcome = toggleTodoError({ message: error.message });
 
-      todoService.toggleTodo.mockReturnValue(throwError(() => error));
+      todoService.toggleTodo = vitest.fn().mockReturnValue(throwError(() => error));
 
       const action = todoEffects.handleToggleTodoSideEffects$;
-      action.pipe(take(1)).subscribe((outputActions) => {
-        expect(outputActions).toEqual(outcome);
-        done();
-      });
+
+      const outputAction = await lastValueFrom(action.pipe(take(1)));
+      expect(outputAction).toEqual(outcome);
     });
   });
 });
