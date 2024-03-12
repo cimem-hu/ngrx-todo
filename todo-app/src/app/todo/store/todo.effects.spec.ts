83d2a975ea5e9da5251c6f63fa58dc0001c0cd96
Rolcho <roland@cimem.hu>
2024-03-12 20:29:12 +0100

    more effects test

--- todo-app/src/app/todo/store/todo.effects.spec.ts
+++ todo-app/src/app/todo/store/todo.effects.spec.ts
@@ -46,4 +46,41 @@ describe("Todo Effects", () => {
       });
     });
   });
+  describe("Remove Todo", () => {
+    beforeEach(() => {
+      todoService = {
+        removeTodo: jest.fn()
+      } as unknown as jest.Mocked<TodoService>;
+
+      const removeTodo = { id: 1 };
+      actions$ = new Actions(of(TodoActions.removeTodoStarted(removeTodo)));
+      todoEffects = new TodoEffects(actions$, todoService);
+    });
+
+    it("should dispatch removeTodoSuccess when todoService.removeTodo is successful", (done) => {
+      const todo = { id: 1 };
+      const dispatchedAction = TodoActions.removeTodoSuccess(todo);
+
+      todoService.removeTodo.mockReturnValue(of(todo));
+
+      const action = todoEffects.handleRemoveTodoSideEffects$;
+      action.pipe(take(1)).subscribe((nextAction) => {
+        expect(nextAction).toEqual(dispatchedAction);
+        done();
+      });
+    });
+
+    it("should dispatch removeTodoError when todService.removeTodo throws an error", (done) => {
+      const error = new Error("Error");
+      const dispatchedAction = TodoActions.removeTodoError({ message: error.message });
+
+      todoService.removeTodo.mockReturnValue(throwError(() => error));
+
+      const action = todoEffects.handleRemoveTodoSideEffects$;
+      action.pipe(take(1)).subscribe((nextAction) => {
+        expect(nextAction).toEqual(dispatchedAction);
+        done();
+      });
+    });
+  });
 });
