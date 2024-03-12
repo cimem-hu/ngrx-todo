3c4602d6a3bf9e08c01ff8663ac60d21328627a6
Rolcho <roland@cimem.hu>
2024-03-12 19:51:01 +0100

    remove unnecessary input

--- todo-app/src/app/todo/store/todo.effects.ts
+++ todo-app/src/app/todo/store/todo.effects.ts
@@ -1,4 +1,3 @@
-import { inject } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { catchError, exhaustMap, map, of } from "rxjs";
 import { HttpErrorResponse } from "@angular/common/http";
